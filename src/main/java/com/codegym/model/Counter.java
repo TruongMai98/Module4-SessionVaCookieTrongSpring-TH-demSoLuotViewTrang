a91b68592dacf05923913e8ffb0443d276aa59b8
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-12-23 08:01:13 +0700

    [Thực hành] Đếm số lượt view trang

---
+++ src/main/java/com/codegym/model/Counter.java
@@ -0,0 +1,30 @@
+package com.codegym.model;
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public int increment() {
+        return count++;
+    }
+
+    @Override
+    public String toString() {
+        return "Counter{" +
+                "count=" + count +
+                '}';
+    }
+}
